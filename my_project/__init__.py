86c889115ca9dcc975be17e6c5eb15a266141c7f
Josh Terry <42044307+Terryboy1996@users.noreply.github.com>
2022-03-07 19:12:18 +0000

    Add files via upload

---
+++ my_project/__init__.py
@@ -0,0 +1,26 @@
+import os
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'mysecretkey'
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+
+#created database
+db = SQLAlchemy(app)
+#abilities to migrate that db
+Migrate(app,db)
+
+db.create_all()
+db.session.commit()
+
+from my_project.relationship.views import relationship_blueprint
+
+app.register_blueprint(relationship_blueprint, url_prefix='')
