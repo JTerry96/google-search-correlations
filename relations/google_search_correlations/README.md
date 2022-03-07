72726b2b14f79c6b5cd7637b797a3fb2b9498c78
Josh Terry <42044307+Terryboy1996@users.noreply.github.com>
2022-03-07 19:11:44 +0000

    Delete relations directory

--- relations/google_search_correlations/README.md
+++
@@ -1,2 +0,0 @@
-# google_search_correlations
- A flask based web app to help find the relationships between certain google search terms. Use of the google trends api to collect the data.
--- relations/my_project/relationship/forms.py
+++
@@ -1,9 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
-
-class InputForm(FlaskForm):
-
-    input_one = StringField('First input: ', validators = [DataRequired()])
-    input_two = StringField('Second input: ', validators = [DataRequired()])
-    submit = SubmitField('Search')
